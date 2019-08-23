378772d9eee1bf4ef258a7df4fc1b3b12e95b244
bartek <barteksulkowski1@gmail.com>
2019-08-23 11:16:54 +0200

    back from sub menu to menu with clear display

--- controller/common.py
+++ controller/common.py
@@ -4,7 +4,7 @@ implement commonly used functions here
 from model.store import store
 from view import terminal_view
 import os
-
+from controller import root_controller
 def display_clear():
     os.system('cls||clear')
 
@@ -14,10 +14,10 @@ def basic_submenu():
                 "(2) Add to table",
                 "(3) Remove from table",
                 "(4) Update table",
-                "(0) Return to main menu"]
-    
+                "(6) Return to main menu"]
+    argument = None
     choice = None
-    while choice != "0":
+    while argument != "0":
         choice = terminal_view.get_choice(options)
         if choice == "1":
             store.show(table)
@@ -27,5 +27,8 @@ def basic_submenu():
             store.remove(table, id_)
         elif choice == "4":
             store.update(table, id_, record)
+        elif choice == "6":
+            os.system('cls||clear')
+            root_controller.run()
         else:
             terminal_view.print_error_message("There is no such choice.")
